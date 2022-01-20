78ac61a1732524bb9e8480abba76484998c1d1ca
Dipali Ahire <ahire.dipali5@gmail.com>
2022-01-20 11:37:31 +0530

    project created

---
+++ Project6/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project6
+//
+//  Created by Dipali Rajbanshi on 20/01/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
